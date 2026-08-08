165268dd720f1e9db0d7efb971c5469ac487b7b9
Ismayil Garayev <149945594+ismayilqarayev@users.noreply.github.com>
2026-08-09 00:37:19 +0400

    Refactor Main class and remove abstract Student class

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,24 +1,9 @@
 package com.example;
 import java.util.Scanner;
 
-abstract class Student{
-    private String name;
-    private String surname;
-    private String phoneNumber;
-    private String email;
-
-    public Student(String name, String surname, String phoneNumber, String email){
-        this.name = name;
-        this.surname = surname;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-    }
-}
-
-public class Main {
+class Main {
     public static void main(String[] args){
-        try(Scanner scanner = new Scanner(System.in)){
 
-        }
+        
     }
 }
--- java/data.java
+++ java/data.java
@@ -1,3 +1,4 @@
-public class data {
-    
+
+class data {
+
 }
